700eb326d2295d29c38af457f478f3c81c856de6
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-03-16 23:45:54 +0000

    Making mobile device code

--- src/com/company/TwoFactorAuthentication.java
+++ src/com/company/TwoFactorAuthentication.java
@@ -5,29 +5,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class TwoFactorAuthentication extends Authentication{
+
+    //Sends the code to a txt file
+    private void sendCode(Integer code){
+        try{
+            FileWriter myWriter = new FileWriter("MobileDeviceCode.txt");
+            myWriter.write("Your code: " + code.toString());
+            myWriter.close();
+        }catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
     public boolean loginAttempt(User a, String b) {
         Random rand = new Random();
 
         //Generate number between 0-999
-        int generatedCode = rand.nextInt(1000);
-        
-        try {
-            File myObj = new File("filename.txt");
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
+        Integer generatedCode = rand.nextInt(1000);
+        String generatedCodeString = generatedCode.toString();
+        sendCode(generatedCode);
 
         Util.print("A code was sent to your mobile device!");
 
         Util.printInLine("Code: ");
         String insertedCode = Util.getLine();
 
-        return false;
+        return insertedCode.strip().equals(generatedCodeString);
     }
 }
